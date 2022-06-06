4f5f8eb65693ad18874ea2a742b9007d4f484c4b
Franco Traverso <franco-t20@outlook.com>
2022-06-06 19:49:45 -0300

    Add geolocation validation

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { MapsApp } from "./MapsApp";
 
+if (!navigator.geolocation) {
+  alert("Your current browser geolocation settings are disabled");
+  throw new Error("Your current browser geolocation settings are disabled");
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
