b6dd16ae4345e22c3d98aa0280450b070192bac4
Franco Traverso <franco-t20@outlook.com>
2022-06-06 19:57:44 -0300

    Add getUserLocation helper function

---
+++ src/helpers/getUserLocation.ts
@@ -0,0 +1,14 @@
+export const getUserLocation = async (): Promise<[number, number]> => {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        resolve([coords.longitude, coords.latitude]);
+      },
+      (err) => {
+        alert("The current positon couldn't be determined.");
+        console.log(err);
+        reject();
+      }
+    );
+  });
+};
