14d79c1a0f089af1ad1ad8b2b28ec0f13225ee4a
Franco Traverso <franco-t20@outlook.com>
2022-06-09 22:34:34 -0300

    Add HomeScreen component

--- src/MapsApp.tsx
+++ src/MapsApp.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { HomeScreen } from "./screens/HomeScreen";
 import { PlacesProvider } from "./context";
 
 export const MapsApp = () => {
   return (
     <PlacesProvider>
       <h1>Maps App</h1>
+      <HomeScreen />
     </PlacesProvider>
   );
 };
