0fdf47602466015d4f1352765db0881d6edc591f
Franco Traverso <franco-t20@outlook.com>
2022-06-11 19:54:52 -0300

    Configure Mapbox

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { MapsApp } from "./MapsApp";
 
+import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZnJhbmNvdHIiLCJhIjoiY2w0YWdzcGZ3MG82YjNwc2V0dmJueWFuYiJ9.o9VxhFgqcUiTNaLGaHBi0Q";
+
 if (!navigator.geolocation) {
   alert("Your current browser geolocation settings are disabled");
   throw new Error("Your current browser geolocation settings are disabled");
