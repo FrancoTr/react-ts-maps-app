95e1046731b8c38d7df25baabd9a963aee35b130
Franco Traverso <franco-t20@outlook.com>
2022-06-10 23:30:46 -0300

    Add MapView component

---
+++ src/components/MapView.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const MapView = () => {
+  return <div>---Geolocation---</div>;
+};
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { MapView } from "../components";
 
 export const HomeScreen = () => {
-  return <div>HomeScreen</div>;
+  return (
+    <div>
+      <MapView />
+    </div>
+  );
 };
