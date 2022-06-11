0544efc2724e034b14376a4970c8beb65a538e2a
Franco Traverso <franco-t20@outlook.com>
2022-06-11 20:07:33 -0300

    Create and display new map

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,13 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useLayoutEffect, useRef } from "react";
+import { Map } from "mapbox-gl";
 import { PlacesContext } from "../context";
 import { Loading } from "./";
 
 export const MapView = () => {
   const { isLoading, userLocation } = useContext(PlacesContext);
+  const mapDiv = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (!isLoading) {
+      const map = new Map({
+        container: mapDiv.current!, // container ID
+        style: "mapbox://styles/mapbox/streets-v11", // style URL
+        center: userLocation, // starting position [lng, lat]
+        zoom: 14, // starting zoom
+      });
+    }
+  }, [isLoading]);
 
   if (isLoading) {
     return <Loading />;
   }
 
-  return <div>{userLocation?.join(",")}</div>;
+  return (
+    <div
+      ref={mapDiv}
+      style={{
+        backgroundColor: "red",
+        height: "100vh",
+        left: 0,
+        position: "fixed",
+        top: 0,
+        width: "100vw",
+      }}
+    >
+      {userLocation?.join(",")}
+    </div>
+  );
 };
