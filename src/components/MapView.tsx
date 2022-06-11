30a7941a9dfee071488f38683c437880009ece22
Franco Traverso <franco-t20@outlook.com>
2022-06-11 19:18:34 -0300

    Add Loading component

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,8 +1,13 @@
 import React, { useContext } from "react";
 import { PlacesContext } from "../context";
+import { Loading } from "./";
 
 export const MapView = () => {
   const { isLoading, userLocation } = useContext(PlacesContext);
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return <div>{userLocation?.join(",")}</div>;
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { MapView } from "./MapView";
+export { Loading } from "./Loading";
