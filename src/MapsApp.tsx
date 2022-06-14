724a9a45e092d8e68e4dbce05d7113bfe7baf769
Franco Traverso <franco-t20@outlook.com>
2022-06-13 22:19:01 -0300

    Implement Map context into root component

--- src/MapsApp.tsx
+++ src/MapsApp.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { HomeScreen } from "./screens";
-import { PlacesProvider } from "./context";
+import { PlacesProvider, MapProvider } from "./context";
 
 import "./globalStyles.css";
 
 export const MapsApp = () => {
   return (
     <PlacesProvider>
-      <h1>Maps App</h1>
-      <HomeScreen />
+      <MapProvider>
+        <HomeScreen />
+      </MapProvider>
     </PlacesProvider>
   );
 };
