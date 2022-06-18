b3a9e53a0a51e1a08d3695c5c5607c2bac59a7e0
Franco Traverso <franco-t20@outlook.com>
2022-06-17 23:53:59 -0300

    Add My Location button functionality

--- src/components/BtnMyLocation.tsx
+++ src/components/BtnMyLocation.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MapContext, PlacesContext } from "../context";
 
 export const BtnMyLocation = () => {
+  const { map, isMapReady } = useContext(MapContext);
+  const { userLocation } = useContext(PlacesContext);
+
+  const onClick = () => {
+    if (!isMapReady) throw new Error("Mapbox is currently unavailable");
+    if (!userLocation) throw new Error("The location could not be established");
+
+    map?.flyTo({ zoom: 14, center: userLocation });
+  };
+
   return (
     <button
       className="btn btn-primary"
+      onClick={onClick}
       style={{ position: "fixed", top: "20px", right: "20px", zIndex: "999" }}
     >
       My Location
