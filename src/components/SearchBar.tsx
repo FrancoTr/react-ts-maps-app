cce064dfef922ffd549ecd71589a0460ef71b944
Franco Traverso <franco-t20@outlook.com>
2022-06-23 23:54:54 -0300

    Add debouncer function to search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { ChangeEvent, useRef } from "react";
 
 export const SearchBar = () => {
+  const debounceRef = useRef<NodeJS.Timeout>();
+
+  const onQueryChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (debounceRef.current) {
+      clearTimeout(debounceRef.current);
+    }
+    debounceRef.current = setTimeout(() => {
+      // Todo
+      console.log("debounced value: ", event.target.value);
+    }, 500);
+  };
+
   return (
     <div className="search-container">
-      <input type="text" className="form-control" placeholder="Search Location..." />
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Search Location..."
+        onChange={onQueryChange}
+      />
     </div>
   );
 };
