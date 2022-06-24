b06354e9ced83c175034191bb32c0ca4716f0447
Franco Traverso <franco-t20@outlook.com>
2022-06-23 23:59:19 -0300

    Add searchApi

---
+++ src/apis/searchApi.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const searchApi = axios.create({
+  baseURL: "https://api.mapbox.com/geocoding/v5/mapbox.places",
+  params: {
+    limit: 5,
+    language: "en",
+    acces_token: "TBD",
+  },
+});
