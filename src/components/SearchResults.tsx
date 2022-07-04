4e7131e235c20f0f596f4e1280328e4c1e7bd5fa
Franco Traverso <franco-t20@outlook.com>
2022-07-03 21:06:31 -0300

    Display each searched place in the search bar

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { PlacesContext } from "../context";
 
 export const SearchResults = () => {
+  const { places, isLoadingPlaces } = useContext(PlacesContext);
   return (
     <ul className="list-group mt-3">
-      <li className="list-group-item list-group-item-action">
-        <h6>Name</h6>
-        <p className="text-muted" style={{ fontSize: "12px" }}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, laborum facere
-          obcaecati.
-        </p>
-        <button className="btn btn-outline-primary">Address</button>
-      </li>
+      {places.map((place) => (
+        <li key={place.id} className="list-group-item list-group-item-action">
+          <h6>Name</h6>
+          <p className="text-muted" style={{ fontSize: "12px" }}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, laborum facere
+            obcaecati.
+          </p>
+          <button className="btn btn-outline-primary">Address</button>
+        </li>
+      ))}
     </ul>
   );
 };
