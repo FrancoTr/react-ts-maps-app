69a176b3c0cae3e525492033e7abad4e2def15e8
Franco Traverso <franco-t20@outlook.com>
2022-07-08 23:47:23 -0300

    Add pointer animation when hovering over the search results

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -16,7 +16,7 @@ export const SearchResults = () => {
   return (
     <ul className="list-group mt-3">
       {places.map((place) => (
-        <li key={place.id} className="list-group-item list-group-item-action">
+        <li key={place.id} className="list-group-item list-group-item-action pointer">
           <h6>{place.text}</h6>
           <p className="text-muted" style={{ fontSize: "12px" }}>
             {place.place_name}
