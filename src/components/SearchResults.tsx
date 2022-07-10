4a5b45fdc822753df8a9a13e1878c852ef729ca7
Franco Traverso <franco-t20@outlook.com>
2022-07-10 19:28:21 -0300

    Center on a new place when clicking it

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,9 +1,19 @@
 import React, { useContext } from "react";
-import { PlacesContext } from "../context";
+import { MapContext, PlacesContext } from "../context";
 import { LoadingPlaces } from "./";
+import { Feature } from "../interfaces/places";
 
 export const SearchResults = () => {
   const { places, isLoadingPlaces } = useContext(PlacesContext);
+  const { map } = useContext(MapContext);
+
+  const onPlaceClicked = (place: Feature) => {
+    const [lng, lat] = place.center;
+    map?.flyTo({
+      zoom: 14,
+      center: [lng, lat],
+    });
+  };
 
   if (isLoadingPlaces) {
     return <LoadingPlaces />;
@@ -16,7 +26,11 @@ export const SearchResults = () => {
   return (
     <ul className="list-group mt-3">
       {places.map((place) => (
-        <li key={place.id} className="list-group-item list-group-item-action pointer">
+        <li
+          key={place.id}
+          className="list-group-item list-group-item-action pointer"
+          onClick={() => onPlaceClicked(place)}
+        >
           <h6>{place.text}</h6>
           <p className="text-muted" style={{ fontSize: "12px" }}>
             {place.place_name}
